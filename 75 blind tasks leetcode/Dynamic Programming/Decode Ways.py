537d855f331a7b5052346a0a72d57ad3b7d94e5d
raj <tiwaridevesh419@gmail.com>
2023-11-23 13:15:06 +0530

    commit

---
+++ 75 blind tasks leetcode/Dynamic Programming/Decode Ways.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        
+        for i in range(1, n+1):
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+            
+            if i > 1 and s[i-2:i] >= '10' and s[i-2:i] <= '26':
+                dp[i] += dp[i-2]
+        
+        return dp[n]
