78680ef477503b40b56707eab309ac0a891ad94a
raj <tiwaridevesh419@gmail.com>
2023-11-27 10:40:00 +0530

    commited

---
+++ 75 blind tasks leetcode/Graph/7. Graph Valid Tree.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        from collections import defaultdict
+        graph = defaultdict(list)
+        
+        # build the graph
+        for src, dest in edges:
+            graph[src].append(dest)
+            graph[dest].append(src)
+            
+        visited = set()
+        def dfs(root, parent): # returns true if graph has no cycle
+            visited.add(root)
+            for node in graph[root]:
+                if node == parent: # trivial cycle, skip
+                    continue
+                if node in visited:
+                    return False
+            
+                if not dfs(node, root):
+                    return False
+            return True
+        
+        return dfs(0, -1) and len(visited) == n
