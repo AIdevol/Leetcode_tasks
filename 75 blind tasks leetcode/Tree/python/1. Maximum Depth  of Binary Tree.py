8b9fecc76459b26520800316f6b226655d5a49ec
raj <tiwaridevesh419@gmail.com>
2023-12-07 23:37:06 +0530

    commited

---
+++ 75 blind tasks leetcode/Tree/python/1. Maximum Depth  of Binary Tree.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 
