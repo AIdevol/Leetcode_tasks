8de379fea9e6edde9deee9c3d2a25d769151f5fe
raj <tiwaridevesh419@gmail.com>
2023-12-07 23:08:55 +0530

    commited

---
+++ 75 blind tasks leetcode/String/python/10. Palindromic Substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        count = 0
+        dp = [[False] * n for _ in range(n)]
+
+        # All single characters are palindromic
+        for i in range(n):
+            dp[i][i] = True
+            count += 1
+
+        # Check for palindromic substrings of length 2
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                count += 1
+
+        # Check for palindromic substrings of length 3 or more
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if dp[i + 1][j - 1] and s[i] == s[j]:
+                    dp[i][j] = True
+                    count += 1
+
+        return count
