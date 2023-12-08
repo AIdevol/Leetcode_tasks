7140499127db13426bb5f55bdb9137c94be96e22
raj <tiwaridevesh419@gmail.com>
2023-12-08 19:09:13 +0530

    commited

---
+++ 75 blind tasks leetcode/Tree/python/2. Same Tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        while p and q:
+            if(p.val==q.val):
+                return True and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+            else:
+                return False
+        if((p and not q) or (not p and q)):
+            return False
+        else:
+            return True
