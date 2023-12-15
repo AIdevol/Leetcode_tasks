ec7d1792eac36f3f69be3c70b10973aa95bc510b
raj <tiwaridevesh419@gmail.com>
2023-12-15 22:17:59 +0530

    commited

---
+++ Top interview 150/1. merge sorted array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        v3 = []
+        i, j = 0, 0
+
+        while i < m and j < n:
+            if nums1[i] > nums2[j]:
+                v3.append(nums2[j])
+                j += 1
+            else:
+                v3.append(nums1[i])
+                i += 1
+
+        while i < m:
+            v3.append(nums1[i])
+            i += 1
+
+        while j < n:
+            v3.append(nums2[j])
+            j += 1
+
+        nums1[:] = v3
