a1a9fcb7f607e9d53c08361534610061b30e541e
deveshtiwari <tiwaridevesh419@gmail.com>
2024-01-07 20:45:30 +0530

    commited

--- Top interview 150/11. H-Index.py
+++ Top interview 150/11. H-Index.py
@@ -1,10 +1,11 @@
- citations.sort(reverse=True)  # Sort the array in descending order
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h_index = 0
+        for i, citation in enumerate(citations):
+            if citation >= i + 1:
+                h_index = i + 1
+            else:
+                break
 
-    h_index = 0
-    for i, citation in enumerate(citations):
-        if citation >= i + 1:
-            h_index = i + 1
-        else:
-            break
-
-    return h_index
+        return h_index
