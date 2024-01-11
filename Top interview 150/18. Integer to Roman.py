e3f7eaa27dbad4fe4b4e4af22eb68cd3ee04ccc8
deveshtiwari <tiwaridevesh419@gmail.com>
2024-01-11 14:48:49 +0530

    commited

--- Top interview 150/18. Integer to Roman.py
+++ Top interview 150/18. Integer to Roman.py
@@ -1,4 +1,12 @@
-val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
         rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
 
         ans = ""
+        for i in range(13):
+            while N >= val[i]:
+                ans += rom[i]
+                N -= val[i]
+
+        return ans
