7687417243cf3fc7a08d84d77d55d70d75ba60f3
devesh <tiwaridevesh419@gmail.com>
2024-01-19 09:09:59 +0530

    updated

---
+++ Top interview 150/21. Reverse Words in a String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s=s.strip()
+        words = s.split()
+        words = words[::-1]
+        reversed_string = ' '.join(words)
+        return reversed_string
