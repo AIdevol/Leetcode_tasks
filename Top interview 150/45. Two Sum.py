c0a52dd1950fa31f19393b52b01b807201ba547b
devesh <tiwaridevesh419@gmail.com>
2024-02-08 11:16:15 +0530

    commited

---
+++ Top interview 150/45. Two Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_indices = {}
+    
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], i]
+            num_indices[num] = i
