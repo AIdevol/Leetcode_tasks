4d5d227c1c7738a7a63ca820d087fa4df6505eba
devesh <tiwaridevesh419@gmail.com>
2024-02-26 10:53:38 +0530

    POTD_2024

---
+++ POTD_2024/January/1. Assign Cookies.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+            
+        i, j = 0, 0
+        content_children = 0
+        
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                content_children += 1
+                i += 1
+            j += 1
+        
+        return content_children
