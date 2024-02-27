496dd0f321f49f59bdc72bf1f5673206112cb49e
devesh <tiwaridevesh419@gmail.com>
2024-02-27 10:57:23 +0530

    updated

---
+++ Top interview 150/68. LRU Cache.py
@@ -0,0 +1,51 @@
+class ListNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_node(self, node):
+        prev = node.prev
+        new = node.next
+        prev.next = new
+        new.prev = prev
+
+    def _move_to_front(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._move_to_front(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._move_to_front(node)
+        else:
+            if len(self.cache) == self.capacity:
+                del self.cache[self.tail.prev.key]
+                self._remove_node(self.tail.prev)
+            new_node = ListNode(key, value)
+            self.cache[key] = new_node
+            self._add_node(new_node)
