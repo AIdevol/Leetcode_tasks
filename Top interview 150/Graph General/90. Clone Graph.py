80acf36059b046072552bf8466efafb50e3b9025
devesh <tiwaridevesh419@gmail.com>
2024-03-16 13:19:46 +0530

    graph general

---
+++ Top interview 150/Graph General/90. Clone Graph.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: Optional[Node]) -> Optional[Node]:
+        if not node:
+            return None
+        
+        visited = {}
+        return self.cloneGraphHelper(node, visited)
+    
+    def cloneGraphHelper(self, node: Node, visited: dict) -> Node:
+        if node in visited:
+            return visited[node]
+        
+        copy = Node(node.val)
+        visited[node] = copy
+        
+        for neighbor in node.neighbors:
+            neighborCopy = self.cloneGraphHelper(neighbor, visited)
+            copy.neighbors.append(neighborCopy)
+        
+        return copy
