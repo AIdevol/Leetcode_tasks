1f558faec9cf3e575b23717b99a444ee7f2c9245
devesh <tiwaridevesh@gmail.com>
2024-04-12 00:51:07 +0530

    3rd feb

---
+++ POTD_2024/February/3. Partition Array for Maximum Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            max_val = 0
+            for j in range(1, min(k, i) + 1):
+                max_val = max(max_val, arr[i - j])
+                dp[i] = max(dp[i], dp[i - j] + max_val * j)
+
+        return dp[n]
